feee256ea517459e131db22f394e7fcae8ac93b9
Imalka <Imalka.emedia@gmail.com>
2021-01-30 21:45:59 +0530

    Initital

---
+++ Plex/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Plex
+//
+//  Created by Imalka Muthukumara on 2021-01-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Google: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Google.layer.cornerRadius = 80
+    }
+   
+
+
+}
+
